0536e7de2cd7cb555d7f4e72c916f74aedf09f61
chuck <c.burnell@gmail.com>
2018-01-02 16:59:41 -0700

    working basic setup for debian

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Do this before everything else
+#su -
+#usermod -aG sudo cburn
+#exit
+
+sudo apt -y install aptitude
+sudo aptitude -y update
+sudo aptitude -y upgrade
+sudo aptitude -y install git neovim i3 i3status suckless-tools build-essential zsh curl python-stdeb 
+
+chsh -s /bin/zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sudo pypi-install py3status
+if [ -f ~/.config/i3/config ]; then
+	echo "removing i3 config"
+	rm ~/.config/i3/config
+fi
+ln -s `pwd`/dotfiles/i3/config ~/.config/i3/config
+
+if [ -f ~/.zshrc ]; then
+	echo "removing ~/.zshrc"
+	rm ~/.zshrc
+fi
+ln -s `pwd`/dotfiles/.zshrc ~/.zshrc
+
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
