f26def3d7b5121361485de9027f3f9a0cb810912
chuck <c.burnell@gmail.com>
2018-01-03 15:25:21 -0700

    removed weird swap file i hope?

--- aptitude-install.sh
+++ aptitude-install.sh
@@ -1,4 +1,4 @@
 sudo aptitude -y update
 sudo aptitude -y upgrade
-sudo aptitude -y install git neovim i3 i3status suckless-tools build-essential zsh curl python-stdeb texlive-full ranger 
+sudo aptitude -y install git neovim i3 i3status suckless-tools build-essential zsh curl python-stdeb texlive-full ranger
 sudo apt-get -y install python-neovim python3-pip 
